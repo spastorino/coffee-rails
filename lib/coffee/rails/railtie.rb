59e4f75be3ef064f6e2ee9210d359684589804da
José Valim <jose.valim@gmail.com>
2011-06-29 12:28:38 -0700

    This should not happen in an initializer. An initializer is just executed after config/application.rb is read. This means this would override whatever default set by the user. This commit fix it.

--- lib/coffee/rails/railtie.rb
+++ lib/coffee/rails/railtie.rb
@@ -1,7 +1,5 @@
 module Coffee::Rails
   class Railtie < ::Rails::Railtie
-    initializer :setup_coffee do
-      config.app_generators.javascript_engine 'coffee'
-    end
+    config.app_generators.javascript_engine :coffee
   end
 end
